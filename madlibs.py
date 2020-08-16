be4768b2359251cdcb87507e9cb6bb79c61b7d7f
Ghad1ee2r3 <52378925+Ghad1ee2r3@users.noreply.github.com>
2020-08-16 14:14:22 +0300

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,17 +1,16 @@
 def main():
-	# write your code here
- time= input("Enter a number from 1 to 12:")
- items= input("Enter a noun (plural):")
- name=input("Enter a name: ")
+	time= input("Enter a number from 1 to 12:")
+        items= input("Enter a noun (plural):")
+        name=input("Enter a name: ")
 	#When printing the name to the user, the first letter of each word should be capitalized. See the example above.
- scream= input("Enter any sentence: ")
+        scream= input("Enter any sentence: ")
 	      #When printing the scream to the user, all the letters in the scream have to be capitalized. See the example above.
- action=input("Enter a verb:")
+         action=input("Enter a verb:")
 
- print("The story goes...")
+        print("The story goes...")
 
- print ("It was "+time+" o'clock when I heard a knock at the door.I opened the door and there was a box full of" +items+" with a note saying From Mr. "+name.capitalize() +."Just as I closed the door I heard a scream "+scream..upper()+
-       ".I froze in place and all I could do was"+ action)
+        print ("It was "+time+" o'clock when I heard a knock at the door.I opened the door and there was a box full of" +items+" with a note saying From Mr. "+name.capitalize() +."Just as I closed the door I heard a scream "+scream..upper()+
+          ".I froze in place and all I could do was"+ action)
 
 
 if __name__ == '__main__':
